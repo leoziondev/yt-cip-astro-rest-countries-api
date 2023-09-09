294a55e9d072a6ab9d81c768d71578b4d175222a
leoziondev <leozionsuporte@gmail.com>
2023-09-09 19:27:18 -0300

    config colors and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"sans": ["Nunito Sans", "system-ui"]
+			},
+			colors: {
+				theme: {
+					bg: "hsl(var(--theme-bg) / <alpha-value>)",
+					elements: "hsl(var(--theme-elements) / <alpha-value>)",
+					text: "hsl(var(--theme-text) / <alpha-value>)",
+					input: "hsl(var(--theme-input) / <alpha-value>)",
+				}
+			}
+		},
 	},
 	plugins: [],
 }
